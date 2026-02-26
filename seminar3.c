da21ff4cc223041489bd0f9a6256459c8cf604cb
perryvibes <iulian.perijoc1111@gmail.com>
2026-02-26 21:56:45 +0200

    update

--- seminar3.c
+++ seminar3.c
@@ -14,20 +14,55 @@ struct StructuraMasina {
 	char* numeSofer;
 	unsigned char serie;
 };
+
 typedef struct StructuraMasina Masina;
 
 void afisareMasina(Masina masina) {
-	//afiseaza toate atributele unei masini
+	printf("\n\nId: %d", masina.id);
+	printf("\nNr Usi: %d", masina.nrUsi);
+	printf("\nPret: %.2f", masina.pret);
+	printf("\nModel: %s", masina.model);
+	printf("\nNume Sofer: %s", masina.numeSofer);
+	printf("\nSerie: %c", masina.serie);
+}
+
+Masina InitializareMasina(int _id, int _nrUsi, float _pret, char* _model, char* _numeSofer, unsigned char _serie) { // testare
+	
+	Masina m;
+	m.id = _id;
+	m.nrUsi = _nrUsi;
+	m.pret = _pret;
+	m.model = malloc((sizeof(char) * strlen(_model)) + 1);
+	strcpy_s(m.model, (sizeof(char)*strlen(_model))+1, _model);
+	m.numeSofer = malloc((sizeof(char) * strlen(_numeSofer)) + 1);
+	strcpy_s(m.numeSofer, (sizeof(char)*strlen(_numeSofer))+1, _numeSofer);
+	m.serie = _serie;
+
+	return m;
 }
 
 void afisareVectorMasini(Masina* masini, int nrMasini) {
 	//afiseaza toate elemente de tip masina din vector
 	//prin apelarea functiei afisareMasina()
+	for (int i = 0; i < nrMasini; i++) {
+		afisareMasina(masini[i]);
+	}
 }
 
-void adaugaMasinaInVector(Masina** masini, int * nrMasini, Masina masinaNoua) {
+void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
 	//adauga in vectorul primit o noua masina pe care o primim ca parametru
 	//ATENTIE - se modifica numarul de masini din vector;
+
+	Masina* copie = malloc(sizeof(Masina) * ((*nrMasini) + 1));
+	for (int i = 0; i < *nrMasini; i++) {
+		copie[i] = (*masini)[i];
+	}
+	copie[*nrMasini] = masinaNoua;
+	(*nrMasini)++;
+	if ((*masini) != NULL) {
+		free((*masini));
+	}
+	(*masini) = copie;
 }
 
 Masina citireMasinaFisier(FILE* file) {
@@ -47,6 +82,23 @@ void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
 }
 
 int main() {
+
+	Masina m1 = InitializareMasina(1, 3, 5000, "Astra", "Ionescu", 'A');
+	Masina m2 = InitializareMasina(2, 5, 8000, "Mokka", "Vasilescu", 'S');
+	Masina m3 = InitializareMasina(3, 4, 9000, "Passat", "Gigel", 'I');
+	
+	Masina* masini;
+	int nrMasini = 2;
+	masini = malloc(sizeof(Masina) * nrMasini);
+	masini[0] = m1;
+	masini[1] = m2;
+	afisareMasina(masini[0]);
+	afisareMasina(masini[1]);
+
+	printf("\n---------------------------------------------------");
+
+	adaugaMasinaInVector(&masini, &nrMasini, m3);
+	afisareMasina(masini[2]);
 
 
 	return 0;

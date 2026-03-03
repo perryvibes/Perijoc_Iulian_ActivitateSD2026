a9dcd1fe9a255f5fb3b9d0fd3ac9d03e40fe6e33
perryvibes <iulian.perijoc1111@gmail.com>
2026-03-03 19:42:53 +0200

    update

--- seminar3.c
+++ seminar3.c
@@ -68,6 +68,24 @@ void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
 Masina citireMasinaFisier(FILE* file) {
 	//functia citeste o masina dintr-un strceam deja deschis
 	//masina citita este returnata;
+	Masina m;
+	char buffer[100];
+	char delimitator[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* token;
+	token = strtok(buffer, delimitator);
+	m.id = atoi(token);
+	m.nrUsi = atoi(strtok(NULL, delimitator));
+	m.pret = atof(strtok(NULL, delimitator));
+	token = strtok(NULL, delimitator);
+	m.model = malloc(sizeof(char) * (strlen(token) + 1));
+	strcpy_s(m.model, strlen(token) + 1, token);
+	token = strtok(NULL, delimitator);
+	m.numeSofer = malloc(sizeof(char) * (strlen(token) + 1));
+	strcpy_s(m.numeSofer, strlen(token) + 1, token);
+	token = strtok(NULL, delimitator);
+	m.serie = token[0];
+	return m;
 }
 
 Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
@@ -75,31 +93,71 @@ Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
 	//prin apelul repetat al functiei citireMasinaFisier()
 	//numarul de masini este determinat prin numarul de citiri din fisier
 	//ATENTIE - la final inchidem fisierul/stream-ul
+
+	FILE* fptr = fopen(numeFisier, "r");
+	
+	if (!fptr) {
+		printf("Eroare deschidere fisier %s\n", numeFisier);
+		return NULL;
+	}
+	else {
+		Masina* masini = NULL;
+		while (!feof(fptr)) {
+			adaugaMasinaInVector(&masini, nrMasiniCitite, citireMasinaFisier(fptr));
+		}
+		fclose(fptr);
+		return masini;
+	}
 }
 
 void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
 	//este dezalocat intreg vectorul de masini
+	if ((*vector) != NULL) {
+		for (int i = 0; i < *nrMasini; i++) {
+			free((*vector)[i].numeSofer);
+			free((*vector)[i].model);
+		}
+		free(*vector);
+		(*vector) = NULL;
+		*nrMasini = 0;
+	}
 }
 
 int main() {
 
-	Masina m1 = InitializareMasina(1, 3, 5000, "Astra", "Ionescu", 'A');
-	Masina m2 = InitializareMasina(2, 5, 8000, "Mokka", "Vasilescu", 'S');
-	Masina m3 = InitializareMasina(3, 4, 9000, "Passat", "Gigel", 'I');
+	//Masina m1 = InitializareMasina(1, 3, 5000, "Astra", "Ionescu", 'A');
+	//Masina m2 = InitializareMasina(2, 5, 8000, "Mokka", "Vasilescu", 'S');
+	//Masina m3 = InitializareMasina(3, 4, 9000, "Passat", "Gigel", 'I');
+	//
+	//Masina* masini;
+	//int nrMasini = 2;
+	//masini = malloc(sizeof(Masina) * nrMasini);
+	//masini[0] = m1;
+	//masini[1] = m2;
+	//afisareMasina(masini[0]);
+	//afisareMasina(masini[1]);
+
+	//printf("\n---------------------------------------------------");
+	//
+	//adaugaMasinaInVector(&masini, &nrMasini, m3);
+	//afisareMasina(masini[2]);
+
+	//printf("\n---------------------------------------------------");
+
+	////FILE* file;
+	////file = fopen("masini.txt", "r");
+
+	////printf("\n%s", citireMasinaFisier(file).numeSofer);
+
+	//printf("\n---------------------------------------------------");
 	
-	Masina* masini;
-	int nrMasini = 2;
-	masini = malloc(sizeof(Masina) * nrMasini);
-	masini[0] = m1;
-	masini[1] = m2;
-	afisareMasina(masini[0]);
-	afisareMasina(masini[1]);
-
+	Masina* masini = NULL;
+	int nrMasini = 0;
+	
+	masini = citireVectorMasiniFisier("masini.txt", &nrMasini);
+	afisareVectorMasini(masini, nrMasini);
+	
 	printf("\n---------------------------------------------------");
-
-	adaugaMasinaInVector(&masini, &nrMasini, m3);
-	afisareMasina(masini[2]);
-
 
 	return 0;
 }

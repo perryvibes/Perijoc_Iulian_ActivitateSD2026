23c5019e2eda163453107030ba5746c24b301833
perryvibes <iulian.perijoc1111@gmail.com>
2026-03-11 00:41:17 +0200

    update

--- seminar3.c
+++ seminar3.c
@@ -1,163 +1,163 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-//trebuie sa folositi fisierul masini.txt
-//sau va creati un alt fisier cu alte date
-
-struct StructuraMasina {
-	int id;
-	int nrUsi;
-	float pret;
-	char* model;
-	char* numeSofer;
-	unsigned char serie;
-};
-
-typedef struct StructuraMasina Masina;
-
-void afisareMasina(Masina masina) {
-	printf("\n\nId: %d", masina.id);
-	printf("\nNr Usi: %d", masina.nrUsi);
-	printf("\nPret: %.2f", masina.pret);
-	printf("\nModel: %s", masina.model);
-	printf("\nNume Sofer: %s", masina.numeSofer);
-	printf("\nSerie: %c", masina.serie);
-}
-
-Masina InitializareMasina(int _id, int _nrUsi, float _pret, char* _model, char* _numeSofer, unsigned char _serie) { // testare
-	
-	Masina m;
-	m.id = _id;
-	m.nrUsi = _nrUsi;
-	m.pret = _pret;
-	m.model = malloc((sizeof(char) * strlen(_model)) + 1);
-	strcpy_s(m.model, (sizeof(char)*strlen(_model))+1, _model);
-	m.numeSofer = malloc((sizeof(char) * strlen(_numeSofer)) + 1);
-	strcpy_s(m.numeSofer, (sizeof(char)*strlen(_numeSofer))+1, _numeSofer);
-	m.serie = _serie;
-
-	return m;
-}
-
-void afisareVectorMasini(Masina* masini, int nrMasini) {
-	//afiseaza toate elemente de tip masina din vector
-	//prin apelarea functiei afisareMasina()
-	for (int i = 0; i < nrMasini; i++) {
-		afisareMasina(masini[i]);
-	}
-}
-
-void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
-	//adauga in vectorul primit o noua masina pe care o primim ca parametru
-	//ATENTIE - se modifica numarul de masini din vector;
-
-	Masina* copie = malloc(sizeof(Masina) * ((*nrMasini) + 1));
-	for (int i = 0; i < *nrMasini; i++) {
-		copie[i] = (*masini)[i];
-	}
-	copie[*nrMasini] = masinaNoua;
-	(*nrMasini)++;
-	if ((*masini) != NULL) {
-		free((*masini));
-	}
-	(*masini) = copie;
-}
-
-Masina citireMasinaFisier(FILE* file) {
-	//functia citeste o masina dintr-un strceam deja deschis
-	//masina citita este returnata;
-	Masina m;
-	char buffer[100];
-	char delimitator[3] = ",\n";
-	fgets(buffer, 100, file);
-	char* token;
-	token = strtok(buffer, delimitator);
-	m.id = atoi(token);
-	m.nrUsi = atoi(strtok(NULL, delimitator));
-	m.pret = atof(strtok(NULL, delimitator));
-	token = strtok(NULL, delimitator);
-	m.model = malloc(sizeof(char) * (strlen(token) + 1));
-	strcpy_s(m.model, strlen(token) + 1, token);
-	token = strtok(NULL, delimitator);
-	m.numeSofer = malloc(sizeof(char) * (strlen(token) + 1));
-	strcpy_s(m.numeSofer, strlen(token) + 1, token);
-	token = strtok(NULL, delimitator);
-	m.serie = token[0];
-	return m;
-}
-
-Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
-	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
-	//prin apelul repetat al functiei citireMasinaFisier()
-	//numarul de masini este determinat prin numarul de citiri din fisier
-	//ATENTIE - la final inchidem fisierul/stream-ul
-
-	FILE* fptr = fopen(numeFisier, "r");
-	
-	if (!fptr) {
-		printf("Eroare deschidere fisier %s\n", numeFisier);
-		return NULL;
-	}
-	else {
-		Masina* masini = NULL;
-		while (!feof(fptr)) {
-			adaugaMasinaInVector(&masini, nrMasiniCitite, citireMasinaFisier(fptr));
-		}
-		fclose(fptr);
-		return masini;
-	}
-}
-
-void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
-	//este dezalocat intreg vectorul de masini
-	if ((*vector) != NULL) {
-		for (int i = 0; i < *nrMasini; i++) {
-			free((*vector)[i].numeSofer);
-			free((*vector)[i].model);
-		}
-		free(*vector);
-		(*vector) = NULL;
-		*nrMasini = 0;
-	}
-}
-
-int main() {
-
-	//Masina m1 = InitializareMasina(1, 3, 5000, "Astra", "Ionescu", 'A');
-	//Masina m2 = InitializareMasina(2, 5, 8000, "Mokka", "Vasilescu", 'S');
-	//Masina m3 = InitializareMasina(3, 4, 9000, "Passat", "Gigel", 'I');
-	//
-	//Masina* masini;
-	//int nrMasini = 2;
-	//masini = malloc(sizeof(Masina) * nrMasini);
-	//masini[0] = m1;
-	//masini[1] = m2;
-	//afisareMasina(masini[0]);
-	//afisareMasina(masini[1]);
-
-	//printf("\n---------------------------------------------------");
-	//
-	//adaugaMasinaInVector(&masini, &nrMasini, m3);
-	//afisareMasina(masini[2]);
-
-	//printf("\n---------------------------------------------------");
-
-	////FILE* file;
-	////file = fopen("masini.txt", "r");
-
-	////printf("\n%s", citireMasinaFisier(file).numeSofer);
-
-	//printf("\n---------------------------------------------------");
-	
-	Masina* masini = NULL;
-	int nrMasini = 0; 
-	
-	masini = citireVectorMasiniFisier("masini.txt", &nrMasini);
-	afisareVectorMasini(masini, nrMasini);
-	//dezalocareVectorMasini(&masini, nrMasini); // Eroare exit code -...... Din cauza nrMasini = 0 
-	printf("\n---------------------------------------------------");
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+////trebuie sa folositi fisierul masini.txt
+////sau va creati un alt fisier cu alte date
+//
+//struct StructuraMasina {
+//	int id;
+//	int nrUsi;
+//	float pret;
+//	char* model;
+//	char* numeSofer;
+//	unsigned char serie;
+//};
+//
+//typedef struct StructuraMasina Masina;
+//
+//void afisareMasina(Masina masina) {
+//	printf("\n\nId: %d", masina.id);
+//	printf("\nNr Usi: %d", masina.nrUsi);
+//	printf("\nPret: %.2f", masina.pret);
+//	printf("\nModel: %s", masina.model);
+//	printf("\nNume Sofer: %s", masina.numeSofer);
+//	printf("\nSerie: %c", masina.serie);
+//}
+//
+//Masina InitializareMasina(int _id, int _nrUsi, float _pret, char* _model, char* _numeSofer, unsigned char _serie) { // testare
+//	
+//	Masina m;
+//	m.id = _id;
+//	m.nrUsi = _nrUsi;
+//	m.pret = _pret;
+//	m.model = malloc((sizeof(char) * strlen(_model)) + 1);
+//	strcpy_s(m.model, (sizeof(char)*strlen(_model))+1, _model);
+//	m.numeSofer = malloc((sizeof(char) * strlen(_numeSofer)) + 1);
+//	strcpy_s(m.numeSofer, (sizeof(char)*strlen(_numeSofer))+1, _numeSofer);
+//	m.serie = _serie;
+//
+//	return m;
+//}
+//
+//void afisareVectorMasini(Masina* masini, int nrMasini) {
+//	//afiseaza toate elemente de tip masina din vector
+//	//prin apelarea functiei afisareMasina()
+//	for (int i = 0; i < nrMasini; i++) {
+//		afisareMasina(masini[i]);
+//	}
+//}
+//
+//void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
+//	//adauga in vectorul primit o noua masina pe care o primim ca parametru
+//	//ATENTIE - se modifica numarul de masini din vector;
+//
+//	Masina* copie = malloc(sizeof(Masina) * ((*nrMasini) + 1));
+//	for (int i = 0; i < *nrMasini; i++) {
+//		copie[i] = (*masini)[i];
+//	}
+//	copie[*nrMasini] = masinaNoua;
+//	(*nrMasini)++;
+//	if ((*masini) != NULL) {
+//		free((*masini));
+//	}
+//	(*masini) = copie;
+//}
+//
+//Masina citireMasinaFisier(FILE* file) {
+//	//functia citeste o masina dintr-un strceam deja deschis
+//	//masina citita este returnata;
+//	Masina m;
+//	char buffer[100];
+//	char delimitator[3] = ",\n";
+//	fgets(buffer, 100, file);
+//	char* token;
+//	token = strtok(buffer, delimitator);
+//	m.id = atoi(token);
+//	m.nrUsi = atoi(strtok(NULL, delimitator));
+//	m.pret = atof(strtok(NULL, delimitator));
+//	token = strtok(NULL, delimitator);
+//	m.model = malloc(sizeof(char) * (strlen(token) + 1));
+//	strcpy_s(m.model, strlen(token) + 1, token);
+//	token = strtok(NULL, delimitator);
+//	m.numeSofer = malloc(sizeof(char) * (strlen(token) + 1));
+//	strcpy_s(m.numeSofer, strlen(token) + 1, token);
+//	token = strtok(NULL, delimitator);
+//	m.serie = token[0];
+//	return m;
+//}
+//
+//Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
+//	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+//	//prin apelul repetat al functiei citireMasinaFisier()
+//	//numarul de masini este determinat prin numarul de citiri din fisier
+//	//ATENTIE - la final inchidem fisierul/stream-ul
+//
+//	FILE* fptr = fopen(numeFisier, "r");
+//	
+//	if (!fptr) {
+//		printf("Eroare deschidere fisier %s\n", numeFisier);
+//		return NULL;
+//	}
+//	else {
+//		Masina* masini = NULL;
+//		while (!feof(fptr)) {
+//			adaugaMasinaInVector(&masini, nrMasiniCitite, citireMasinaFisier(fptr));
+//		}
+//		fclose(fptr);
+//		return masini;
+//	}
+//}
+//
+//void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
+//	//este dezalocat intreg vectorul de masini
+//	if ((*vector) != NULL) {
+//		for (int i = 0; i < *nrMasini; i++) {
+//			free((*vector)[i].numeSofer);
+//			free((*vector)[i].model);
+//		}
+//		free(*vector);
+//		(*vector) = NULL;
+//		*nrMasini = 0;
+//	}
+//}
+//
+//int main() {
+//
+//	//Masina m1 = InitializareMasina(1, 3, 5000, "Astra", "Ionescu", 'A');
+//	//Masina m2 = InitializareMasina(2, 5, 8000, "Mokka", "Vasilescu", 'S');
+//	//Masina m3 = InitializareMasina(3, 4, 9000, "Passat", "Gigel", 'I');
+//	//
+//	//Masina* masini;
+//	//int nrMasini = 2;
+//	//masini = malloc(sizeof(Masina) * nrMasini);
+//	//masini[0] = m1;
+//	//masini[1] = m2;
+//	//afisareMasina(masini[0]);
+//	//afisareMasina(masini[1]);
+//
+//	//printf("\n---------------------------------------------------");
+//	//
+//	//adaugaMasinaInVector(&masini, &nrMasini, m3);
+//	//afisareMasina(masini[2]);
+//
+//	//printf("\n---------------------------------------------------");
+//
+//	////FILE* file;
+//	////file = fopen("masini.txt", "r");
+//
+//	////printf("\n%s", citireMasinaFisier(file).numeSofer);
+//
+//	//printf("\n---------------------------------------------------");
+//	
+//	Masina* masini = NULL;
+//	int nrMasini = 0; 
+//	
+//	masini = citireVectorMasiniFisier("masini.txt", &nrMasini);
+//	afisareVectorMasini(masini, nrMasini);
+//	//dezalocareVectorMasini(&masini, nrMasini); // Eroare exit code -...... Din cauza nrMasini = 0 
+//	printf("\n---------------------------------------------------");
+//
+//	return 0;
+//}
--- seminar4.c
+++ seminar4.c
@@ -1,94 +1,233 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-////trebuie sa folositi fisierul masini.txt
-////sau va creati un alt fisier cu alte date
-//
-//struct StructuraMasina {
-//	int id;
-//	int nrUsi;
-//	float pret;
-//	char* model;
-//	char* numeSofer;
-//	unsigned char serie;
-//};
-//typedef struct StructuraMasina Masina;
-//
-////creare structura pentru un nod dintr-o lista simplu inlantuita
-//
-//Masina citireMasinaDinFisier(FILE* file) {
-//	char buffer[100];
-//	char sep[3] = ",\n";
-//	fgets(buffer, 100, file);
-//	char* aux;
-//	Masina m1;
-//	aux = strtok(buffer, sep);
-//	m1.id = atoi(aux);
-//	m1.nrUsi = atoi(strtok(NULL, sep));
-//	m1.pret= atof(strtok(NULL, sep));
-//	aux = strtok(NULL, sep);
-//	m1.model = malloc(strlen(aux) + 1);
-//	strcpy_s(m1.model, strlen(aux) + 1, aux);
-//
-//	aux = strtok(NULL, sep);
-//	m1.numeSofer = malloc(strlen(aux) + 1);
-//	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
-//
-//	m1.serie = *strtok(NULL, sep);
-//	return m1;
-//}
-//
-//void afisareMasina(Masina masina) {
-//	printf("Id: %d\n", masina.id);
-//	printf("Nr. usi : %d\n", masina.nrUsi);
-//	printf("Pret: %.2f\n", masina.pret);
-//	printf("Model: %s\n", masina.model);
-//	printf("Nume sofer: %s\n", masina.numeSofer);
-//	printf("Serie: %c\n\n", masina.serie);
-//}
-//
-//void afisareListaMasini(/*lista de masini*/) {
-//	//afiseaza toate elemente de tip masina din lista simplu inlantuita
-//	//prin apelarea functiei afisareMasina()
-//}
-//
-//void adaugaMasinaInLista(/*lista de masini*/ Masina masinaNoua) {
-//	//adauga la final in lista primita o noua masina pe care o primim ca parametru
-//}
-//
-//void adaugaLaInceputInLista(/*lista de masini*/ Masina masinaNoua) {
-//	//adauga la inceputul listei o noua masina pe care o primim ca parametru
-//}
-//
-//void* citireListaMasiniDinFisier(const char* numeFisier) {
-//	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
-//	//prin apelul repetat al functiei citireMasinaDinFisier()
-//	//ATENTIE - la final inchidem fisierul/stream-ul
-//}
-//
-//void dezalocareListaMasini(/*lista de masini*/) {
-//	//sunt dezalocate toate masinile si lista de elemente
-//}
-//
-//float calculeazaPretMediu(/*lista de masini*/) {
-//	//calculeaza pretul mediu al masinilor din lista.
-//	return 0;
-//}
-//
-//void stergeMasiniDinSeria(/*lista masini*/ char serieCautata) {
-//	//sterge toate masinile din lista care au seria primita ca parametru.
-//	//tratati situatia ca masina se afla si pe prima pozitie, si pe ultima pozitie
-//}
-//
-//float calculeazaPretulMasinilorUnuiSofer(/*lista masini*/ const char* numeSofer) {
-//	//calculeaza pretul tuturor masinilor unui sofer.
-//	return 0;
-//}
-//
-//int main() {
-//
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//trebuie sa folositi fisierul masini.txt
+//sau va creati un alt fisier cu alte date
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+
+typedef struct StructuraMasina Masina;
+typedef struct Nod Nod;
+
+struct Nod {
+	Masina info;
+	Nod* next;
+};
+
+
+//creare structura pentru un nod dintr-o lista simplu inlantuita
+
+Masina citireMasinaDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	aux = strtok(buffer, sep);
+	m1.id = atoi(aux);
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret= atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+
+	m1.serie = *strtok(NULL, sep);
+	return m1;
+}
+
+void afisareMasina(Masina masina) {
+	printf("Id: %d\n", masina.id);
+	printf("Nr. usi : %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n\n", masina.serie);
+}
+
+void afisareListaMasini(Nod* cap) {
+	//afiseaza toate elemente de tip masina din lista simplu inlantuita
+	//prin apelarea functiei afisareMasina()
+	while (cap) {
+		afisareMasina(cap->info);
+		cap = cap->next; // daca nu exista alt nod va fi NULL
+	}
+}
+
+void adaugaMasinaInLista(Nod** cap, Masina masinaNoua) {
+	//adauga la final in lista primita o noua masina pe care o primim ca parametru
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = masinaNoua;
+	nou->next = NULL;
+	if ((*cap) == NULL) {
+		(*cap) = nou;
+	}
+	else {
+		Nod* p = (*cap);
+		while (p->next != NULL) {
+			p = p->next;
+		}
+		p->next = nou;
+	}
+
+}
+
+void adaugaLaInceputInLista(Nod** cap, Masina masinaNoua) {
+	//adauga la inceputul listei o noua masina pe care o primim ca parametru
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = masinaNoua;
+	nou->next = cap;
+	(*cap) = nou;
+}
+
+void* citireListaMasiniDinFisier(const char* numeFisier) {
+	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+	//prin apelul repetat al functiei citireMasinaDinFisier()
+	//ATENTIE - la final inchidem fisierul/stream-ul
+	FILE* fptr = fopen(numeFisier, "r");
+	Nod* lista = NULL;
+	while (!feof(fptr)) {
+		adaugaMasinaInLista(&lista, citireMasinaDinFisier(fptr));
+	}
+	fclose(fptr);
+	return lista;
+}
+void* citireListaMasiniDinFisier1(const char* numeFisier) {
+	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+	//prin apelul repetat al functiei citireMasinaDinFisier()
+	//ATENTIE - la final inchidem fisierul/stream-ul
+	FILE* fptr = fopen(numeFisier, "r");
+	Nod* lista = NULL;
+	while (!feof(fptr)) {
+		adaugaLaInceputInLista(&lista, citireMasinaDinFisier(fptr));
+	}
+	fclose(fptr);
+	return lista;
+}
+
+void dezalocareListaMasini(Nod** cap) {
+	//sunt dezalocate toate masinile si lista de elemente
+	while ((*cap) != NULL) {
+		Nod* aux = (*cap);
+		*cap = (*cap)->next;
+		free(aux->info.numeSofer);
+		free(aux->info.model);
+		free(aux);
+	}
+}
+
+float calculeazaPretMediu(Nod* lista) {
+	//calculeaza pretul mediu al masinilor din lista.
+	if (!lista) {
+		return 0;
+	}
+	int count = 0;
+	float suma = 0;
+	while (lista) {
+		suma += lista->info.pret;
+		count++;
+		lista = lista->next;
+	}
+	return suma / count;
+}
+
+void stergeMasiniDinSeria(Nod** lista, char serieCautata) {
+	//sterge toate masinile din lista care au seria primita ca parametru.
+	//tratati situatia ca masina se afla si pe prima pozitie, si pe ultima pozitie
+
+	Nod* p = *lista;
+	while (p != NULL && p->info.serie == serieCautata) {
+		Nod* sters = p;
+		p = p->next;
+
+		free(sters->info.numeSofer);
+		free(sters->info.model);
+		free(sters);
+	}
+	*lista = p;
+	if (p != NULL) {
+		Nod* ant = p;
+		Nod* curent = p->next;
+		while (curent != NULL) {
+			if (curent->info.serie == serieCautata) {
+				Nod* sters = curent;
+				ant->next = curent->next;
+				curent = curent->next;
+				free(sters->info.numeSofer);
+				free(sters->info.model);
+				free(sters);
+			}
+			else {
+				ant = curent;
+				curent = curent->next;
+			}
+		}
+	}
+}
+
+float calculeazaPretulMasinilorUnuiSofer(Nod* lista, const char* numeSofer) {
+	//calculeaza pretul tuturor masinilor unui sofer.
+	float suma = 0;
+	while (lista) {
+		if (strcmp(numeSofer, lista->info.numeSofer) == 0) {
+			suma += lista->info.pret;
+		}
+		lista = lista->next;
+	}
+	return suma;
+}
+
+char* getCeaMaiScumpaMasina(Nod* lista) {
+	float pretulMaxim = 0;
+	char* modelScump = NULL;
+	while (lista) {
+		if (lista->info.pret > pretulMaxim) {
+			pretulMaxim = lista->info.pret;
+			modelScump = lista->info.model;
+		}
+		lista = lista->next;
+	}
+	if (pretulMaxim > 0) {
+		char* nou = malloc(strlen(modelScump));
+		strcpy_s(nou, strlen(modelScump) + 1, modelScump);
+		return nou;
+	}
+	else {
+		return NULL;
+	}
+}
+
+int main() {
+	Nod* cap = NULL;
+	cap = citireListaMasiniDinFisier("masini2.txt");
+	afisareListaMasini(cap);
+
+	float pretMediu = calculeazaPretMediu(cap);
+	printf("Pret mediu este: %5.2f\n", pretMediu);
+
+	float pretSofer = calculeazaPretulMasinilorUnuiSofer(cap, "Ionescu");
+	printf("Pretul masinilor lui Ionescu este: %5.2f", pretSofer);
+
+	printf("\n%s", getCeaMaiScumpaMasina(cap));
+
+	char serieCautata = 'A';
+
+	stergeMasiniDinSeria(&cap, serieCautata);
+	printf("Seria dupa stergere: \n");
+	afisareListaMasini(cap);
+	
+	dezalocareListaMasini(&cap);
+	return 0;
+}

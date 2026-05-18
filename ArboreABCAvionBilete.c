7059a498823f8ec112401a9c8caa23f4c8f2d61c
perryvibes <iulian.perijoc1111@gmail.com>
2026-05-18 16:02:55 +0300

    Exercitiu arbore ABC, are erori

---
+++ ArboreABCAvionBilete.c
@@ -0,0 +1,164 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct BiletAvion {
+	int id;
+	float pret;
+	char* destinatie;
+	char* poartaImbarcare;
+} BiletAvion;
+
+typedef struct Nod {
+	BiletAvion info;
+	struct Nod* nodSt;
+	struct Nod* nodDr;
+} Nod;
+
+BiletAvion citireBiletDinFisier(FILE* file) {
+	// atentie! Este citire BILET din fisier si nu citire ARBORE din fisier!
+	BiletAvion bilet;
+	char buffer[100];
+	char separator[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* token = strtok(buffer, separator);
+	bilet.id = atoi(token);
+	token = strtok(NULL, separator);
+	bilet.pret = atof(token);
+	token = strtok(NULL, separator);
+	bilet.destinatie = malloc(sizeof(char) * (1 + strlen(token)));
+	strcpy(bilet.destinatie, token);
+	token = strtok(NULL, separator);
+	bilet.poartaImbarcare = malloc(sizeof(char) * (1 + strlen(token)));
+	strcpy(bilet.poartaImbarcare, token);
+	return bilet;
+}
+
+void afisareBilet(BiletAvion b) {
+	printf("Id:%d\n", b.id);
+	printf("Pret:%5.2f\n", b.pret);
+	printf("Destinatie:%s\n", b.destinatie);
+	printf("Poarta imbarcare:%s\n\n", b.poartaImbarcare);
+}
+
+void adaugaBiletInArbore(Nod** arbore, BiletAvion biletNou) {
+	// trebuie sa ne luam un criteriu pentru adaugare, vom lua pretul.
+	if ((*arbore) == NULL) {
+		Nod* nou = malloc(sizeof(Nod));
+		nou->info = biletNou;
+		nou->nodSt = NULL;
+		nou->nodDr = NULL;
+		(*arbore) = nou;
+	}
+	else {
+		if ((*arbore)->info.pret > biletNou.pret) {
+			adaugaBiletInArbore(&((*arbore)->nodSt), biletNou);
+		}
+		else {
+			adaugaBiletInArbore(&((*arbore)->nodDr), biletNou);
+		}
+	}
+}
+
+Nod* citireArboreDinFisier(const char* numeFisier) {
+	Nod* arbore = NULL;
+	FILE* fptr = fopen(numeFisier, "r");
+	while (!feof(fptr)) {
+		adaugaBiletInArbore(&arbore, citireBiletDinFisier(fptr));
+	}
+	fclose(fptr);
+	return arbore;
+}
+
+void afisareInOrdineSRD(Nod* arbore) {
+	afisareInOrdineSRD(arbore->nodSt);
+	afisareBilet(arbore->info);
+	afisareInOrdineSRD(arbore->nodDr);
+}
+
+void afisarePreordineRSD(Nod* arbore) {
+	afisareBilet(arbore->info);
+	afisarePreordineRSD(arbore->nodSt);
+	afisarePreordineRSD(arbore->nodDr);
+}
+
+void afisarePostordineSDR(Nod* arbore) {
+	afisarePostordineSDR(arbore->nodSt);
+	afisarePostordineSDR(arbore->nodDr);
+	afisareBilet(arbore->info);
+}
+
+
+void dezalocareArbore(Nod** arbore) {
+	if ((*arbore)) {
+		dezalocareArbore(&((*arbore)->nodSt));
+		dezalocareArbore(&((*arbore)->nodDr));
+		free((*arbore)->info.destinatie);
+		free((*arbore)->info.poartaImbarcare);
+		free((*arbore));
+	}
+}
+
+BiletAvion getBiletById(Nod* arbore, int id) {
+	BiletAvion bilet;
+	if (arbore) {
+		if (arbore->info.id > id) {
+			bilet = getBiletById(arbore->nodSt, id);
+		}
+		else if (arbore->info.id < id) {
+			bilet = getBiletById(arbore->nodDr, id);
+		}
+		else {
+			return arbore->info;
+		}
+	}
+	return bilet;
+}
+
+int determinaNrNoduri(Nod* arbore) {
+	if (arbore) {
+		int nodSt = determinaNrNoduri(arbore->nodSt);
+		int nodDr = determinaNrNoduri(arbore->nodDr);
+		return 1 + nodSt + nodDr;
+	}
+	else {
+		return 0;
+	}
+}
+
+float calculeazaPretTotal(Nod* arbore) {
+	if (arbore) {
+		float st = calculeazaPretTotal(arbore->nodSt);
+		float dr = calculeazaPretTotal(arbore->nodDr);
+		return arbore->info.pret + st + dr;
+	}
+	else {
+		return 0;
+	}
+}
+
+float calculeazaPretTotalDestinatie(Nod* arbore, const char* destinatie) {
+	if (arbore) {
+		float st = calculeazaPretTotal(arbore->nodSt, destinatie);
+		float dr = calculeazaPretTotal(arbore->nodDr, destinatie);
+		if (strcmp(arbore->info.destinatie, destinatie) == 0) {
+			return arbore->info.pret;
+		}
+		else {
+			return st + dr;
+		}
+	}
+	else {
+		return 0;
+	}
+}
+
+int main() {
+
+	Nod* arbore = citireArboreDinFisier("bilete.txt");
+	afisareInOrdineSRD(arbore);
+
+
+	return 0;
+}
--- HeapCocktail.c
+++ HeapCocktail.c
@@ -1,147 +1,147 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-struct Cocktail {
-	int id;
-	int nrIngrediente;
-	float pret;
-	char* denumire;
-	char* origine;
-};
-
-typedef struct Cocktail Cocktail;
-
-typedef struct Heap {
-	Cocktail* vector;
-	int lungime;
-	int nrCocktails;
-} Heap;
-
-void afisareCocktail(Cocktail c) {
-	printf("Id:%d\n", c.id);
-	printf("Numar ingrediente:%d\n", c.nrIngrediente);
-	printf("Pret:%5.2f\n", c.pret);
-	printf("Denumire:%s\n", c.denumire);
-	printf("Origine:%s\n\n", c.origine);
-}
-
-Cocktail citesteCocktailDinFisier(FILE* file) {
-	Cocktail cocktail;
-	char buffer[100];
-	char delimitator[3] = ",\n";
-	fgets(buffer, 100, file);
-	char* token;
-	token = strtok(buffer, delimitator);
-	cocktail.id = atoi(token);
-	token = strtok(NULL, delimitator);
-	cocktail.nrIngrediente = atoi(token);
-	token = strtok(NULL, delimitator);
-	cocktail.pret = atof(token);
-	token = strtok(NULL, delimitator);
-	cocktail.denumire = malloc((strlen(token) + 1) * sizeof(char));
-	strcpy(cocktail.denumire, token);
-	token = strtok(NULL, delimitator);
-	cocktail.origine = malloc((strlen(token) + 1) * sizeof(char));
-	strcpy(cocktail.origine, token);
-	return cocktail;
-}
-
-Heap initializareHeap(int lungime) {
-	Heap heap;
-	heap.lungime = lungime; // total
-	heap.nrCocktails = 0; // vizibile
-	heap.vector = (Cocktail*)malloc(sizeof(Cocktail) * lungime);
-	return heap;
-}
-
-void filtreazaHeap(Heap heap, int pozNod) {
-	int pozSt = 2 * pozNod + 1;
-	int pozDr = 2 * pozNod + 2;
-	int pozMax = pozNod;
-	if (pozSt < heap.nrCocktails && heap.vector[pozSt].id > heap.vector[pozMax].id) {
-		pozMax = pozSt;
-	} else if (pozDr < heap.nrCocktails && heap.vector[pozDr].id > heap.vector[pozMax].id) {
-		pozMax = pozDr;
-	}
-	if (pozMax != pozNod) {
-		Cocktail aux = heap.vector[pozMax];
-		heap.vector[pozMax] = heap.vector[pozNod];
-		heap.vector[pozNod] = aux;
-		// verificam din nou daca avem situatie dupa aceasta schimbare
-		if (pozMax < (heap.nrCocktails - 2) / 2) // formula pentru ultimul parinte din arbore
-		{
-			filtreazaHeap(heap, pozMax);
-		}
-	}
-}
-
-void afiseazaHeap(Heap heap) { // cocktails vizibile!
-	for (int i = 0; i < heap.nrCocktails; i++) {
-		afisareCocktail(heap.vector[i]);
-	}
-}
-
-void afiseazaHeapAscuns(Heap heap) {
-	for (int i = heap.nrCocktails; i < heap.lungime; i++) {
-		afisareCocktail(heap.vector[i]);
-	}
-}
-
-Heap citesteHeapDinFisier(const char* numeFisier) {
-	Heap heap = initializareHeap(4);
-	FILE* fptr = fopen(numeFisier, "r");
-	while (!feof(fptr)) {
-		// nrCocktails e 0!!!
-		heap.vector[heap.nrCocktails++] = citesteCocktailDinFisier(fptr);
-	}
-	fclose(fptr);
-	for (int i = (heap.nrCocktails - 2) / 2; i >= 0; i--) {
-		filtreazaHeap(heap, i);
-	}
-	return heap;
-}
-
-Cocktail extrageCocktail(Heap* heap) {
-	Cocktail aux;
-	if (heap->lungime > 0) {
-		aux = heap->vector[0];
-		heap->vector[0] = heap->vector[heap->nrCocktails - 1];
-		heap->vector[heap->nrCocktails - 1] = aux;
-		heap->nrCocktails--;
-		for (int i = (heap->nrCocktails - 2) / 2; i >= 0; i--) {
-			filtreazaHeap(*heap, i);
-		}
-	}
-	return aux;
-}
-
-void dezalocareHeap(Heap* heap) {
-	if (heap) {
-		for (int i = 0; i < heap->lungime; i++) {
-			free(heap->vector[i].denumire);
-			free(heap->vector[i].origine);
-		}
-		free(heap->vector);
-		heap->vector = NULL;
-		heap->lungime = 0;
-		heap->nrCocktails = 0;
-	}
-}
-
-int main() {
-	Heap h = citesteHeapDinFisier("cocktails.txt");
-	afiseazaHeap(h);
-
-	printf("\n=================\n");
-	afiseazaHeapAscuns(h);
-
-	printf("\n=================\n");
-	Cocktail c1 = extrageCocktail(&h);
-	afisareCocktail(c1);
-
-	printf("\n=================\n");
-	dezalocareHeap(&h);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<string.h>
+//#include<stdlib.h>
+//
+//struct Cocktail {
+//	int id;
+//	int nrIngrediente;
+//	float pret;
+//	char* denumire;
+//	char* origine;
+//};
+//
+//typedef struct Cocktail Cocktail;
+//
+//typedef struct Heap {
+//	Cocktail* vector;
+//	int lungime;
+//	int nrCocktails;
+//} Heap;
+//
+//void afisareCocktail(Cocktail c) {
+//	printf("Id:%d\n", c.id);
+//	printf("Numar ingrediente:%d\n", c.nrIngrediente);
+//	printf("Pret:%5.2f\n", c.pret);
+//	printf("Denumire:%s\n", c.denumire);
+//	printf("Origine:%s\n\n", c.origine);
+//}
+//
+//Cocktail citesteCocktailDinFisier(FILE* file) {
+//	Cocktail cocktail;
+//	char buffer[100];
+//	char delimitator[3] = ",\n";
+//	fgets(buffer, 100, file);
+//	char* token;
+//	token = strtok(buffer, delimitator);
+//	cocktail.id = atoi(token);
+//	token = strtok(NULL, delimitator);
+//	cocktail.nrIngrediente = atoi(token);
+//	token = strtok(NULL, delimitator);
+//	cocktail.pret = atof(token);
+//	token = strtok(NULL, delimitator);
+//	cocktail.denumire = malloc((strlen(token) + 1) * sizeof(char));
+//	strcpy(cocktail.denumire, token);
+//	token = strtok(NULL, delimitator);
+//	cocktail.origine = malloc((strlen(token) + 1) * sizeof(char));
+//	strcpy(cocktail.origine, token);
+//	return cocktail;
+//}
+//
+//Heap initializareHeap(int lungime) {
+//	Heap heap;
+//	heap.lungime = lungime; // total
+//	heap.nrCocktails = 0; // vizibile
+//	heap.vector = (Cocktail*)malloc(sizeof(Cocktail) * lungime);
+//	return heap;
+//}
+//
+//void filtreazaHeap(Heap heap, int pozNod) {
+//	int pozSt = 2 * pozNod + 1;
+//	int pozDr = 2 * pozNod + 2;
+//	int pozMax = pozNod;
+//	if (pozSt < heap.nrCocktails && heap.vector[pozSt].id > heap.vector[pozMax].id) {
+//		pozMax = pozSt;
+//	} else if (pozDr < heap.nrCocktails && heap.vector[pozDr].id > heap.vector[pozMax].id) {
+//		pozMax = pozDr;
+//	}
+//	if (pozMax != pozNod) {
+//		Cocktail aux = heap.vector[pozMax];
+//		heap.vector[pozMax] = heap.vector[pozNod];
+//		heap.vector[pozNod] = aux;
+//		// verificam din nou daca avem situatie dupa aceasta schimbare
+//		if (pozMax < (heap.nrCocktails - 2) / 2) // formula pentru ultimul parinte din arbore
+//		{
+//			filtreazaHeap(heap, pozMax);
+//		}
+//	}
+//}
+//
+//void afiseazaHeap(Heap heap) { // cocktails vizibile!
+//	for (int i = 0; i < heap.nrCocktails; i++) {
+//		afisareCocktail(heap.vector[i]);
+//	}
+//}
+//
+//void afiseazaHeapAscuns(Heap heap) {
+//	for (int i = heap.nrCocktails; i < heap.lungime; i++) {
+//		afisareCocktail(heap.vector[i]);
+//	}
+//}
+//
+//Heap citesteHeapDinFisier(const char* numeFisier) {
+//	Heap heap = initializareHeap(4);
+//	FILE* fptr = fopen(numeFisier, "r");
+//	while (!feof(fptr)) {
+//		// nrCocktails e 0!!!
+//		heap.vector[heap.nrCocktails++] = citesteCocktailDinFisier(fptr);
+//	}
+//	fclose(fptr);
+//	for (int i = (heap.nrCocktails - 2) / 2; i >= 0; i--) {
+//		filtreazaHeap(heap, i);
+//	}
+//	return heap;
+//}
+//
+//Cocktail extrageCocktail(Heap* heap) {
+//	Cocktail aux;
+//	if (heap->lungime > 0) {
+//		aux = heap->vector[0];
+//		heap->vector[0] = heap->vector[heap->nrCocktails - 1];
+//		heap->vector[heap->nrCocktails - 1] = aux;
+//		heap->nrCocktails--;
+//		for (int i = (heap->nrCocktails - 2) / 2; i >= 0; i--) {
+//			filtreazaHeap(*heap, i);
+//		}
+//	}
+//	return aux;
+//}
+//
+//void dezalocareHeap(Heap* heap) {
+//	if (heap) {
+//		for (int i = 0; i < heap->lungime; i++) {
+//			free(heap->vector[i].denumire);
+//			free(heap->vector[i].origine);
+//		}
+//		free(heap->vector);
+//		heap->vector = NULL;
+//		heap->lungime = 0;
+//		heap->nrCocktails = 0;
+//	}
+//}
+//
+//int main() {
+//	Heap h = citesteHeapDinFisier("cocktails.txt");
+//	afiseazaHeap(h);
+//
+//	printf("\n=================\n");
+//	afiseazaHeapAscuns(h);
+//
+//	printf("\n=================\n");
+//	Cocktail c1 = extrageCocktail(&h);
+//	afisareCocktail(c1);
+//
+//	printf("\n=================\n");
+//	dezalocareHeap(&h);
+//	return 0;
+//}

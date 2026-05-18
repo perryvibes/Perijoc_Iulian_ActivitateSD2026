f7ce1c860d752c546d43b5b750d98225a1fdc630
perryvibes <iulian.perijoc1111@gmail.com>
2026-05-18 23:34:16 +0300

    Problema Piloti arbore, fara punctul cu stiva.

--- ArboreAVLPictura.c
+++ ArboreAVLPictura.c
@@ -1,167 +1,167 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-
-struct Pictura {
-	int id;
-	float pret;
-	char* centruExpozitie;
-	char* numeAutor;
-};
-typedef struct Pictura Pictura;
-
-typedef struct Nod {
-	Pictura info;
-	struct Nod* nodSt;
-	struct Nod* nodDr;
-} Nod;
-
-void afiseazaPictura(Pictura p) {
-	printf("Id:%d\n", p.id);
-	printf("Pret:%5.2f\n", p.pret);
-	printf("Centru expozitie:%s\n", p.centruExpozitie);
-	printf("Nume autor:%s\n\n", p.numeAutor);
-}
-
-
-Pictura citestePicturaDinFisier(FILE* file) {
-	Pictura pictura;
-	char buffer[100];
-	char sep[3] = ",\n";
-	fgets(buffer, 100, file);
-	char* token = strtok(buffer, sep);
-	pictura.id = atoi(token);
-	token = strtok(NULL, sep);
-	pictura.pret = atof(token);
-	token = strtok(NULL, sep);
-	pictura.centruExpozitie = malloc(strlen(token) + 1);
-	strcpy(pictura.centruExpozitie, token);
-	token = strtok(NULL, sep);
-	pictura.numeAutor = malloc(strlen(token) + 1);
-	strcpy(pictura.numeAutor, token);
-	return pictura;
-}
-
-
-int calculInaltime(Nod* radacina) {
-	if (radacina) {
-		return 1 + max(calculInaltime(radacina->nodSt), calculInaltime(radacina->nodDr));
-	}
-}
-
-int GE(Nod* radacina) {
-	if (radacina) {
-		return calculInaltime(radacina->nodSt) - calculInaltime(radacina->nodDr);
-	}
-	else {
-		return 0;
-	}
-
-}
-
-void rotireStanga(Nod** radacina) {
-	if (*radacina) {
-		Nod* aux = (*radacina)->nodDr;
-		(*radacina)->nodDr = aux->nodSt;
-		aux->nodSt = (*radacina);
-		(*radacina) = aux;
-	}
-}
-
-void rotireDreapta(Nod** radacina) {
-	if (*radacina) {
-		Nod* aux = (*radacina)->nodSt;
-		(*radacina)->nodSt = aux->nodDr;
-		aux->nodDr = (*radacina);
-		(*radacina) = aux;
-	}
-}
-
-void adaugaPicturaInArbore(Nod** arbore, Pictura picturaNoua) {
-	if ((*arbore) == NULL) {
-		Nod* nou = malloc(sizeof(Nod));
-		nou->info = picturaNoua;
-		nou->nodSt = NULL;
-		nou->nodDr = NULL;
-		(*arbore) = nou;
-	}
-	else {
-		if ((*arbore)->info.id > picturaNoua.id) {
-			adaugaPicturaInArbore(&((*arbore)->nodSt), picturaNoua);
-		}
-		else if ((*arbore)->info.id < picturaNoua.id) {
-			adaugaPicturaInArbore(&((*arbore)->nodDr), picturaNoua);
-		}
-		if (GE(*arbore) == 2) {
-			if (GE((*arbore)->nodSt) != 1) {
-				rotireStanga(&((*arbore)->nodSt));
-			}
-			rotireDreapta(arbore);
-		}
-		else if (GE(*arbore) == -2) {
-			if (GE((*arbore)->nodDr) != -1) {
-				rotireDreapta(&((*arbore)->nodDr));
-			}
-			rotireStanga(arbore);
-		}
-	}
-}
-
-
-Nod* citesteArboreDinFisier(const char* numeFisier) {
-	Nod* arbore = NULL;
-	FILE* fptr = fopen(numeFisier, "r");
-	while (!feof(fptr)) {
-		adaugaPicturaInArbore(&arbore, citestePicturaDinFisier(fptr));
-	}
-	fclose(fptr);
-	return arbore;
-}
-
-
-void afisareArboreAVL(Nod* arbore) {  //SRD
-	if (arbore) {
-		afisareArboreAVL(arbore->nodSt);
-		afiseazaPictura(arbore->info);
-		afisareArboreAVL(arbore->nodDr);
-	}
-}
-
-void dezalocaArbore(Nod** arbore) {
-	if (*arbore) {
-		dezalocaArbore(&((*arbore)->nodSt));
-		dezalocaArbore(&((*arbore)->nodDr));
-		free((*arbore)->info.centruExpozitie);
-		free((*arbore)->info.numeAutor);
-		free(*arbore);
-		*arbore = NULL;
-	}
-}
-
-Pictura getPicturaById(Nod* arbore, int id) {
-	
-}
-
-int determinaNrNoduri(Nod* arbore) {
-	
-}
-
-float calculeazaPretTotal(Nod* arbore) {
-	
-}
-
-float calculeazaPretTotalCentru(Nod* arbore, const char* centru) {
-	
-}
-
-int main() {
-	Nod* arbore = citesteArboreDinFisier("picturi.txt");
-	afisareArboreAVL(arbore);
-
-	dezalocaArbore(&arbore);
-	printf("\n==========\n");
-
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<string.h>
+//#include<stdlib.h>
+//
+//
+//struct Pictura {
+//	int id;
+//	float pret;
+//	char* centruExpozitie;
+//	char* numeAutor;
+//};
+//typedef struct Pictura Pictura;
+//
+//typedef struct Nod {
+//	Pictura info;
+//	struct Nod* nodSt;
+//	struct Nod* nodDr;
+//} Nod;
+//
+//void afiseazaPictura(Pictura p) {
+//	printf("Id:%d\n", p.id);
+//	printf("Pret:%5.2f\n", p.pret);
+//	printf("Centru expozitie:%s\n", p.centruExpozitie);
+//	printf("Nume autor:%s\n\n", p.numeAutor);
+//}
+//
+//
+//Pictura citestePicturaDinFisier(FILE* file) {
+//	Pictura pictura;
+//	char buffer[100];
+//	char sep[3] = ",\n";
+//	fgets(buffer, 100, file);
+//	char* token = strtok(buffer, sep);
+//	pictura.id = atoi(token);
+//	token = strtok(NULL, sep);
+//	pictura.pret = atof(token);
+//	token = strtok(NULL, sep);
+//	pictura.centruExpozitie = malloc(strlen(token) + 1);
+//	strcpy(pictura.centruExpozitie, token);
+//	token = strtok(NULL, sep);
+//	pictura.numeAutor = malloc(strlen(token) + 1);
+//	strcpy(pictura.numeAutor, token);
+//	return pictura;
+//}
+//
+//
+//int calculInaltime(Nod* radacina) {
+//	if (radacina) {
+//		return 1 + max(calculInaltime(radacina->nodSt), calculInaltime(radacina->nodDr));
+//	}
+//}
+//
+//int GE(Nod* radacina) {
+//	if (radacina) {
+//		return calculInaltime(radacina->nodSt) - calculInaltime(radacina->nodDr);
+//	}
+//	else {
+//		return 0;
+//	}
+//
+//}
+//
+//void rotireStanga(Nod** radacina) {
+//	if (*radacina) {
+//		Nod* aux = (*radacina)->nodDr;
+//		(*radacina)->nodDr = aux->nodSt;
+//		aux->nodSt = (*radacina);
+//		(*radacina) = aux;
+//	}
+//}
+//
+//void rotireDreapta(Nod** radacina) {
+//	if (*radacina) {
+//		Nod* aux = (*radacina)->nodSt;
+//		(*radacina)->nodSt = aux->nodDr;
+//		aux->nodDr = (*radacina);
+//		(*radacina) = aux;
+//	}
+//}
+//
+//void adaugaPicturaInArbore(Nod** arbore, Pictura picturaNoua) {
+//	if ((*arbore) == NULL) {
+//		Nod* nou = malloc(sizeof(Nod));
+//		nou->info = picturaNoua;
+//		nou->nodSt = NULL;
+//		nou->nodDr = NULL;
+//		(*arbore) = nou;
+//	}
+//	else {
+//		if ((*arbore)->info.id > picturaNoua.id) {
+//			adaugaPicturaInArbore(&((*arbore)->nodSt), picturaNoua);
+//		}
+//		else if ((*arbore)->info.id < picturaNoua.id) {
+//			adaugaPicturaInArbore(&((*arbore)->nodDr), picturaNoua);
+//		}
+//		if (GE(*arbore) == 2) {
+//			if (GE((*arbore)->nodSt) != 1) {
+//				rotireStanga(&((*arbore)->nodSt));
+//			}
+//			rotireDreapta(arbore);
+//		}
+//		else if (GE(*arbore) == -2) {
+//			if (GE((*arbore)->nodDr) != -1) {
+//				rotireDreapta(&((*arbore)->nodDr));
+//			}
+//			rotireStanga(arbore);
+//		}
+//	}
+//}
+//
+//
+//Nod* citesteArboreDinFisier(const char* numeFisier) {
+//	Nod* arbore = NULL;
+//	FILE* fptr = fopen(numeFisier, "r");
+//	while (!feof(fptr)) {
+//		adaugaPicturaInArbore(&arbore, citestePicturaDinFisier(fptr));
+//	}
+//	fclose(fptr);
+//	return arbore;
+//}
+//
+//
+//void afisareArboreAVL(Nod* arbore) {  //SRD
+//	if (arbore) {
+//		afisareArboreAVL(arbore->nodSt);
+//		afiseazaPictura(arbore->info);
+//		afisareArboreAVL(arbore->nodDr);
+//	}
+//}
+//
+//void dezalocaArbore(Nod** arbore) {
+//	if (*arbore) {
+//		dezalocaArbore(&((*arbore)->nodSt));
+//		dezalocaArbore(&((*arbore)->nodDr));
+//		free((*arbore)->info.centruExpozitie);
+//		free((*arbore)->info.numeAutor);
+//		free(*arbore);
+//		*arbore = NULL;
+//	}
+//}
+//
+//Pictura getPicturaById(Nod* arbore, int id) {
+//	
+//}
+//
+//int determinaNrNoduri(Nod* arbore) {
+//	
+//}
+//
+//float calculeazaPretTotal(Nod* arbore) {
+//	
+//}
+//
+//float calculeazaPretTotalCentru(Nod* arbore, const char* centru) {
+//	
+//}
+//
+//int main() {
+//	Nod* arbore = citesteArboreDinFisier("picturi.txt");
+//	afisareArboreAVL(arbore);
+//
+//	dezalocaArbore(&arbore);
+//	printf("\n==========\n");
+//
+//
+//}
---
+++ PilotiArbore.c
@@ -0,0 +1,155 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct Pilot {
+	int id;
+	float greutate;
+	char* echipa;
+	int castiguri; // numarul de castiguri pe care le-a avut pilotul
+} Pilot;
+
+typedef struct Nod {
+	Pilot info;
+	struct Nod* nodSt;
+	struct Nod* nodDr;
+} Nod;
+
+void afisarePilot(Pilot pilot) {
+	printf("\nId pilot: %d", pilot.id);
+	printf("\nGreutate pilot: %.2f", pilot.greutate);
+	printf("\nEchipa pilot: %s", pilot.echipa);
+	printf("\nCastiguri pilot: %d\n", pilot.castiguri);
+}
+
+Pilot citirePilotFisier(FILE* fptr) {
+	Pilot pilot;
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, fptr);
+	char* token = strtok(buffer, sep);
+	pilot.id = atoi(token);
+	token = strtok(NULL, sep);
+	pilot.greutate = atof(token);
+	token = strtok(NULL, sep);
+	pilot.echipa = malloc(strlen(token) + 1);
+	strcpy(pilot.echipa, token);
+	token = strtok(NULL, sep);
+	pilot.castiguri = atoi(token);
+	return pilot;
+}
+
+void inserarePilotArbore(Nod** radacina, Pilot pilotNou) {
+	//inserare pe criteriul id-ului
+	if (*radacina == NULL) {
+		Nod* nou = malloc(sizeof(Nod));
+		nou->info = pilotNou;
+		nou->nodSt = NULL;
+		nou->nodDr = NULL;
+		*radacina = nou;
+	}
+	else {
+		if ((*radacina)->info.id > pilotNou.id) {
+			inserarePilotArbore(&((*radacina)->nodSt), pilotNou);
+		}
+		else if ((*radacina)->info.id < pilotNou.id) {
+			inserarePilotArbore(&((*radacina)->nodDr), pilotNou);
+		}
+	}
+}
+
+Nod* citireArboreFisier(const char* numeFisier) {
+	Nod* arbore = NULL;
+	FILE* fptr = fopen(numeFisier, "r");
+	while (!feof(fptr)) {
+		inserarePilotArbore(&arbore, citirePilotFisier(fptr));
+	}
+	fclose(fptr);
+	return arbore;
+}
+
+void afisareArboreInordine(Nod* arbore) {
+	if (arbore) {
+		afisareArboreInordine(arbore->nodSt);
+		afisarePilot(arbore->info);
+		afisareArboreInordine(arbore->nodDr);
+	}
+}
+
+int nrTotalPiloti(Nod* arbore, const char* echipa) {
+	if (arbore) {
+		int sum = 0;
+		int st = nrTotalPiloti(arbore->nodSt,echipa);
+		int dr = nrTotalPiloti(arbore->nodDr,echipa);
+		if (strcmp(arbore->info.echipa, echipa) == 0) {
+			sum++;
+		}
+		return sum + st + dr;
+	}
+	return 0;
+}
+
+Pilot cautarePilot(Nod* arbore, int id) {
+	Pilot pilot = { 0,0,NULL,0 }; // dupa id.
+	if (arbore) {
+		if (arbore->info.id > id) {
+			pilot = cautarePilot(arbore->nodSt, id);
+		}
+		else if (arbore->info.id < id) {
+			pilot = cautarePilot(arbore->nodDr, id);
+		}
+		else {
+			pilot = arbore->info;
+			pilot.echipa = malloc(strlen(arbore->info.echipa) + 1);
+			strcpy(pilot.echipa, arbore->info.echipa);
+		}
+	}
+	return pilot;
+}
+
+void dezalocare(Nod** arbore) {
+	if (*arbore) {
+		dezalocare(&((*arbore)->nodSt));
+		dezalocare(&((*arbore)->nodDr));
+		free((*arbore)->info.echipa);
+		free(*arbore);
+	}
+}
+
+int celeMaiMulteCastiguri(Nod* arbore) {
+	if (arbore) {
+		int max = 0;
+		max = celeMaiMulteCastiguri(arbore->nodSt);
+		max = celeMaiMulteCastiguri(arbore->nodDr);
+		if (max < arbore->info.castiguri) {
+			max = arbore->info.castiguri;
+		}
+		return max;
+	}
+	else {
+		return 0;
+	}
+}
+
+int main() {
+
+	Nod* arbore = citireArboreFisier("piloti.txt");
+	afisareArboreInordine(arbore);
+	printf("\n==========\n");
+	
+	printf("Nr total piloti: %d", nrTotalPiloti(arbore, "Red"));
+	printf("\n==========\n");
+	
+	Pilot m = cautarePilot(arbore, 4);
+	afisarePilot(m);
+	free(m.echipa);
+	printf("\n==========\n");
+	
+	printf("Cele mai multe castiguri: %d", celeMaiMulteCastiguri(arbore));
+	
+	dezalocare(&arbore);
+
+	return 0;
+}
